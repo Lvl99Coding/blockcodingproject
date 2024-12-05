ef03c8215cfad3cd1b98e2c49a4eec0badcd00d5
Caleb Tarantini <calebtarantini@gmail.com>
2024-12-05 18:57:53 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+let repeat: number[] = []
+basic.showIcon(IconNames.Heart)
+basic.pause(500)
+basic.clearScreen()
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.A)) {
+        repeat = [0, 4]
+        for (let value of repeat) {
+            basic.showString("MERRY CHRISTMAS")
+        }
+    }
 })
